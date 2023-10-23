d9037da85deac5ca6ddd73c43b06ef584e5e318c
RdAnjos <rodrigo.oda@hotmail.com>
2023-10-22 22:04:13 -0300

    BugFix CORS

--- src/main/java/me/dio/SantanderBootcamp2023Application.java
+++ src/main/java/me/dio/SantanderBootcamp2023Application.java
@@ -1,8 +1,12 @@
 package me.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Corrigindo erro de CORS na nossa API publicada em PRD no Railway
+@OpenAPIDefinition(servers = { @Server(url = "/" , description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderBootcamp2023Application {
 
